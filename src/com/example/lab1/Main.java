863bd15a6e302dab75c22aa96b566481d90e4822
yeeun <wjs4199@naver.com>
2021-09-04 12:07:30 +0900

    add comment

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -7,5 +7,7 @@ public class Main {
 		System.out.print("이름 입력 : ");
 		String name = s.next();
 		System.out.println("[" + name + "]님 환영합니다~" );
+		System.out.println("한학기동안 실프1 수업 열심히 들어봐요:)");
+		
 	}
 }
